d04b0fe849ae82a52373c16d88296fc87f97d811
ana11090 <72621705+ana11090@users.noreply.github.com>
2021-05-24 19:34:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,7 @@
         console.log(e);
         const email=e.indexOf("@");
         console.log(email);
-            // if ((email==-1) || (e==""))
-            //  {document.getElementById("myDiv").style.borderColor = "hsl(354, 100%, 66%)";
-            //  let text =document.getElementById("error");          
-            // text.innerHTML="Please provide a valid email address";
-            // x=1;
-            // }
+           
 
             if (((email==-1) || (e==""))  && (x===0))
              {document.getElementById("myDiv").style.borderColor = "hsl(354, 100%, 66%)";
